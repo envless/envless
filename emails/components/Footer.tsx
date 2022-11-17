c4f7e8a517da5874a6bc23f1ad8f517f11b12d3a
Puru D <puru@dahal.me>
2022-11-16 19:26:48 -0600

    Getting stated with sending custom email with MJML

---
+++ emails/components/Footer.tsx
@@ -0,0 +1,19 @@
+import { MjmlSection, MjmlColumn, MjmlText } from "mjml-react";
+import { grayDark, textSm } from "./theme";
+
+export default function Footer() {
+  return (
+    <MjmlSection cssClass="smooth">
+      <MjmlColumn>
+        <MjmlText
+          cssClass="footer"
+          padding="24px 24px 48px"
+          fontSize={textSm}
+          color={grayDark}
+        >
+          © {new Date().getFullYear()} Envless
+        </MjmlText>
+      </MjmlColumn>
+    </MjmlSection>
+  );
+}
