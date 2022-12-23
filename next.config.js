c71461d2096c2984a1ff0ae74b64311f6fd43a29
Puru D <puru@dahal.me>
2022-12-23 00:51:19 -0600

    This changes fixed the previous issue
    * https://github.com/trpc/trpc/issues/3465

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: false,
   images: {
     dangerouslyAllowSVG: true,
     domains: ["avatar.vercel.sh"],
