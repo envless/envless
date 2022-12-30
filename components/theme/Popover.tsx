78884211d831874a21db7e83d59bd75033ed62bf
Puru D <puru@dahal.me>
2022-12-29 22:03:13 -0600

    Add feature to search for project

--- components/theme/Popover.tsx
+++ components/theme/Popover.tsx
@@ -1,35 +1,18 @@
-import { Cross1Icon } from "@radix-ui/react-icons";
+import React from "react";
 import * as PopoverPrimitive from "@radix-ui/react-popover";
 import cx from "classnames";
-import React from "react";
+
+/**
+ * Popover component.
+ *
+ * @param {Props} props - The component's props.
+ * @returns {JSX.Element} The component's JSX element.
+ */
 
 interface Props {
   button: React.ReactNode;
   children: React.ReactNode;
 }
-
-const items = [
-  {
-    id: "width",
-    label: "Width",
-    defaultValue: "100%",
-  },
-  {
-    id: "max-width",
-    label: "Max. width",
-    defaultValue: "300px",
-  },
-  {
-    id: "height",
-    label: "Height",
-    defaultValue: "25px",
-  },
-  {
-    id: "max-height",
-    label: "Max. height",
-    defaultValue: "none",
-  },
-];
 
 const Popover = (props: Props) => {
   const { button, children } = props;
@@ -37,23 +20,20 @@ const Popover = (props: Props) => {
   return (
     <div className="relative inline-block text-left">
       <PopoverPrimitive.Root>
-        <PopoverPrimitive.Trigger asChild>
-          {button}
-        </PopoverPrimitive.Trigger>
+        <PopoverPrimitive.Trigger asChild>{button}</PopoverPrimitive.Trigger>
         <PopoverPrimitive.Content
           align="center"
           sideOffset={4}
           className={cx(
-            "radix-side-top:animate-slide-up radix-side-bottom:animate-slide-down",
+            "mx-2 bg-darker",
             "w-56 rounded shadow-md md:w-56",
-            "bg-darker",
-            "shadow-xl shadow-black ring-2 ring-dark focus:outline-none"
+            "shadow-xl shadow-black ring-2 ring-dark focus:outline-none",
+            "radix-side-top:animate-slide-up radix-side-bottom:animate-slide-down",
           )}
         >
           <PopoverPrimitive.Arrow className="fill-current text-dark" />
 
-          { children }
-
+          {children}
         </PopoverPrimitive.Content>
       </PopoverPrimitive.Root>
     </div>
