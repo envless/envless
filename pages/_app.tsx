b0603e7817c4a708a55629422dd21250599f9cfd
Puru D <puru@dahal.me>
2023-01-04 23:09:52 -0600

    Replace with Inter font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import "../styles/font.css";
 import type { AppProps } from "next/app";
-import { Lexend_Deca } from "@next/font/google";
+import { Inter } from "@next/font/google";
 import { Analytics } from "@vercel/analytics/react";
-const font = Lexend_Deca({ subsets: ["latin"], weight: "400" });
+const font = Inter({ subsets: ["latin"], weight: "400" });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
