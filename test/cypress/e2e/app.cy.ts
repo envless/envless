5714b6c7574e2b38bf6f8e72d8d4b6ccb23bb67a
Samyog Dhital <dhitalsamyog123@gmail.com>
2023-02-12 20:38:34 +0545

    fix: comments on the tests

--- test/cypress/e2e/app.cy.ts
+++ test/cypress/e2e/app.cy.ts
@@ -1,12 +1,9 @@
 describe("Home Page", () => {
   it("should navigate to the about page", () => {
-    // Start from the index page
     cy.visit("/");
 
-    // Find a link with an href attribute containing "blog" and click it
     cy.get('a[href*="blog"]').click();
 
-    // The new url should include "/about"
     cy.url().should("include", "/about");
   });
 });
