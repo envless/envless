8e1ae9af9cd153d16264bc9a63fd29cc4db65b8a
Samyog Dhital <dhitalsamyog123@gmail.com>
2023-03-25 19:57:09 +0545

    chore: refactor, fixes toast on success

--- apps/platform/pages/projects/[slug]/settings/index.tsx
+++ apps/platform/pages/projects/[slug]/settings/index.tsx
@@ -16,7 +16,7 @@ import { showToast } from "@/components/theme/showToast";
  * @param {roleInProject} props.roleInProject - The user role in current project.
  */
 
-interface SettingsPageProps {
+interface Props {
   projects: Project[];
   currentProject: Project;
   roleInProject: UserRole;
@@ -26,17 +26,20 @@ export const SettingsPage = ({
   projects,
   roleInProject,
   currentProject,
-}: SettingsPageProps) => {
+}: Props) => {
   const router = useRouter();
-
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const { mutate: projectGeneralMutation, isLoading } =
     trpc.projects.update.useMutation({
-      onSuccess: (data) => {
+      onSuccess: ({ name }) => {
         showToast({
           type: "success",
-          title: "Project setting updated successfully",
+          title: `Project ${name} setting updated successfully`,
           subtitle: "",
         });
         router.push(router.asPath);
@@ -75,51 +78,48 @@ export const SettingsPage = ({
         currentProject={currentProject}
         currentRole={roleInProject}
       >
-        <>
-          <h3 className="mb-8 text-lg">General</h3>
-          <div className="w-full lg:w-3/5">
-            <form onSubmit={handleSubmit(submitForm)}>
-              <Input
-                name="name"
-                label="Project name"
-                placeholder=""
-                defaultValue={currentProject.name || ""}
-                required={true}
-                register={register}
-                className="w-full"
-                validationSchema={{
-                  required: "Project name is required",
-                }}
-              />
-              <div className="border-dark mb-6 rounded border-2 p-3">
-                <div className="flex items-center justify-between">
-                  <label className="cursor-pointer" htmlFor="auth_2fa">
-                    <h3 className="mb-1 text-sm font-semibold">
-                      Enforce two-factor authentication
-                    </h3>
-                    <Paragraph color="light" size="sm" className="mr-4">
-                      After enabling this feature, all team members should
-                      enable their two-factor authentication to access this
-                      project.
-                    </Paragraph>
-                  </label>
+        <h3 className="mb-8 text-lg">General</h3>
+        <div className="w-full lg:w-3/5">
+          <form onSubmit={handleSubmit(submitForm)}>
+            <Input
+              name="name"
+              label="Project name"
+              defaultValue={currentProject.name || ""}
+              register={register}
+              className="w-full"
+              errors={errors}
+              validationSchema={{
+                required: "Project name is required",
+              }}
+              required
+            />
+            <div className="border-dark mb-6 rounded border-2 p-3">
+              <div className="flex items-center justify-between">
+                <label className="cursor-pointer" htmlFor="auth_2fa">
+                  <h3 className="mb-1 text-sm font-semibold">
+                    Enforce two-factor authentication
+                  </h3>
+                  <Paragraph color="light" size="sm" className="mr-4">
+                    After enabling this feature, all team members should enable
+                    their two-factor authentication to access this project.
+                  </Paragraph>
+                </label>
 
-                  <Toggle
-                    // TODO - create enforce2fa column instead of adding it to settings
-                    // checked={currentProject.settings?.enforce2FA || false}
-                    checked={false}
-                    name="enforce2FA"
-                    register={register}
-                  />
-                </div>
+                <Toggle
+                  // TODO: - create enforce2fa column instead of adding it to settings
+                  // checked={currentProject.settings?.enforce2FA || false}
+                  checked={false}
+                  name="enforce2FA"
+                  register={register}
+                />
               </div>
+            </div>
 
-              <Button type="submit" disabled={isLoading || false}>
-                Save project settings
-              </Button>
-            </form>
-          </div>
-        </>
+            <Button type="submit" disabled={isLoading || false}>
+              Save project settings
+            </Button>
+          </form>
+        </div>
       </ProjectSettings>
     </ProjectLayout>
   );
