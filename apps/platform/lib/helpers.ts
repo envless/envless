e77b42cc7330d48de591ebbc014af1684832fbf8
Samyog Dhital <dhitalsamyog123@gmail.com>
2023-03-25 19:52:08 +0545

    chore: truncate helper function

--- apps/platform/lib/helpers.ts
+++ apps/platform/lib/helpers.ts
@@ -47,3 +47,13 @@ export const toUUID = (value: string) => {
   }
   return array.join("");
 };
+
+/**
+ *
+ * Truncate if the given length exceeds and append "..."
+ * @param string
+ * @param charIndex
+ * @returns string
+ */
+export const truncate = (string: string, charIndex: number) =>
+  string.length > charIndex ? `${string.substring(0, charIndex)}...` : string;
