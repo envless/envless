84bfe1c5d80ed0f718b7242c408cf01a778ee146
chetannn <chetankharel7@gmail.com>
2023-03-25 18:57:25 +0545

    fix: project card design

--- apps/platform/components/icons/LockIcon.tsx
+++ apps/platform/components/icons/LockIcon.tsx
@@ -8,11 +8,11 @@ export default function LockIcon({ className }: { className?: string }) {
       width="24"
       height="24"
       viewBox="0 0 24 24"
-      stroke-width="2"
+      strokeWidth={2}
       stroke="currentColor"
       fill="none"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path d="M11.5 21h-4.5a2 2 0 0 1 -2 -2v-6a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v.5" />
--- apps/platform/components/projects/Projects.tsx
+++ apps/platform/components/projects/Projects.tsx
@@ -1,14 +1,8 @@
 import Link from "next/link";
-import { AlertCircle, GitBranch, Users } from "lucide-react";
+import { GitBranch, ShieldAlert, Users } from "lucide-react";
 import DateTimeAgo from "@/components/DateTimeAgo";
 import { LockIcon } from "@/components/icons";
 import CreateProjectModal from "@/components/projects/CreateProjectModal";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "../theme";
 
 const Projects = ({ ...props }) => {
   const { projects } = props;
@@ -23,38 +17,47 @@ const Projects = ({ ...props }) => {
     return (
       <>
         <Link href={`/projects/${project.slug}`} className="cursor-pointer">
-          <div className="border-darker bg-darker relative w-full rounded-md border-2 p-5 hover:border-teal-300/70">
-            {twoFactorAuth && (
-              <TooltipProvider delayDuration={200}>
-                <Tooltip>
-                  <TooltipTrigger asChild>
-                    <LockIcon className="absolute right-5 inline-block h-5 w-5 text-teal-400" />
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <div className="flex space-x-4">
-                      <AlertCircle className="h-6 w-6 shrink-0 text-teal-300" />
-                      <p className="text-xs">
-                        This project requires two-factor authentication
-                      </p>
-                    </div>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-            )}
-            <h5
-              title={project.name}
-              className="line-clamp-1 text-lightest text-base leading-5"
-            >
-              {project?.name}
-            </h5>
-            <p className="text-light mt-1 text-xs">
-              Created {""}
-              <DateTimeAgo date={project.createdAt} />
-            </p>
-            <div className="mt-8 grid grid-cols-2 gap-8 text-xs">
+          <div className="border-darker bg-darker w-full  rounded-md border-2  p-5 transition duration-300 hover:border-teal-300/70">
+            <div className="flex justify-between gap-2">
               <div>
+                <h5
+                  title={project.name}
+                  className="line-clamp-1 text-lightest max-w-[150px] truncate text-base leading-5"
+                >
+                  {project?.name}
+                </h5>
+                <p className="text-light mt-1 text-xs">
+                  Created {""}
+                  <DateTimeAgo date={project.createdAt} />
+                </p>
+              </div>
+
+              <div>
+                {project.deletedAt ? (
+                  <button
+                    aria-label="You have deleted this project. Click to restore it."
+                    data-balloon-pos="up"
+                    className="rounded-full p-2 text-white transition duration-200 hover:bg-white/25"
+                  >
+                    <ShieldAlert className="h-5 w-5 shrink-0 text-red-600" />
+                  </button>
+                ) : (
+                  twoFactorAuth && (
+                    <button
+                      aria-label="This project requires two-factor authentication"
+                      data-balloon-pos="up"
+                      className="rounded-full p-2 text-white transition duration-200 hover:bg-white/25"
+                    >
+                      <LockIcon className="h-5 w-5 shrink-0 text-teal-300" />
+                    </button>
+                  )
+                )}
+              </div>
+            </div>
+            <div className="mt-8 grid grid-cols-2 gap-8 text-xs">
+              <div className="flex flex-col justify-center">
                 <div className="inline-block">
-                  <Users className="text-lighter inline-block h-5 w-5" />
+                  <Users className="text-lighter inline-block h-4 w-4" />
                   <span className="ml-2 inline-block">
                     {project._count.access}
                   </span>
@@ -62,9 +65,9 @@ const Projects = ({ ...props }) => {
                 <p className="text-light mt-1">Members</p>
               </div>
 
-              <div>
+              <div className="flex flex-col justify-center">
                 <div className="inline-block">
-                  <GitBranch className="text-lighter inline-block h-5 w-5" />
+                  <GitBranch className="text-lighter inline-block h-4 w-4 shrink-0" />
                   <span className="ml-2 inline-block">
                     {project._count.branches}
                   </span>
