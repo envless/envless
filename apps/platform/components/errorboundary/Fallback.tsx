9f1ad8b80083c6ffc98bf29a791023429349b0da
Puru Dahal <dahal@users.noreply.github.com>
2023-03-26 05:28:50 -0500

    feat: getting started with integrations (#299)

--- apps/platform/components/errorboundary/Fallback.tsx
+++ apps/platform/components/errorboundary/Fallback.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import { Transition } from "@headlessui/react";
+import { ArrowLeft } from "lucide-react";
 
 const Fallback = () => {
   const router = useRouter();
@@ -79,21 +80,8 @@ const Fallback = () => {
               router.push("/").then(() => router.reload());
             }}
           >
+            <ArrowLeft className="h-6 w-6" />
             <span>Back to Homepage</span>
-            <svg
-              width="20"
-              height="20"
-              viewBox="0 0 15 15"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
-                fill="currentColor"
-                fillRule="evenodd"
-                clipRule="evenodd"
-              ></path>
-            </svg>
           </div>
         </Transition.Child>
       </Transition.Root>
