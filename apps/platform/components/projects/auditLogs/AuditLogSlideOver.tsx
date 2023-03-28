c633e77407ae98e8dcb89658a61fd37cb698678a
chetannn <chetankharel7@gmail.com>
2023-03-28 20:34:00 +0545

    feat: loaded audit logs data and quick refactoring

---
+++ apps/platform/components/projects/auditLogs/AuditLogSlideOver.tsx
@@ -0,0 +1,67 @@
+import { Download } from "lucide-react";
+import { CopyBlock, dracula } from "react-code-blocks";
+import { SlideOver } from "@/components/theme";
+
+type Props = {
+  auditLogs: any;
+  open: boolean;
+  setOpen: (open: boolean) => void;
+};
+
+export default function AuditLogSideOver({ open, setOpen, auditLogs }: Props) {
+  return (
+    <SlideOver
+      title="Audit Log Detail"
+      size="xl"
+      description="Chetan Kharel performed access.created on 29 July 2022, 21:32"
+      open={open}
+      setOpen={setOpen}
+      submitButtonIcon={
+        <Download className="ml-2 -mr-1 h-5 w-5" aria-hidden="true" />
+      }
+      submitButtonText="Download CSV"
+      onClose={() => setOpen(false)}
+    >
+      <div className="flex h-full flex-col">
+        <div className="flex-1 overflow-y-auto">
+          <div className="divide divide-dark divide-y-2 py-2 px-4 sm:px-6">
+            <dl className="grid grid-cols-2 py-4">
+              <dt className="text-sm leading-relaxed">Created By</dt>
+              <dd className="text-sm">Chetan Kharel</dd>
+            </dl>
+
+            <dl className="grid grid-cols-2 items-center py-4">
+              <dt className="text-sm leading-relaxed">Action Performed</dt>
+              <dd>
+                <span className="bg-lighter text-dark rounded-xl px-2 py-0.5 text-sm font-normal tracking-tight">
+                  access.created
+                </span>
+              </dd>
+            </dl>
+
+            <dl className="grid grid-cols-2 py-4">
+              <dt className="text-sm leading-relaxed">Project</dt>
+              <dd className="text-sm">Untitled X</dd>
+            </dl>
+
+            <dl className="grid grid-cols-2 py-4">
+              <dt className="text-sm leading-relaxed">Created At</dt>
+              <dd className="text-sm">29 July 2022, 9:32 PM</dd>
+            </dl>
+
+            <div className="py-4">
+              <label className="mt-1 text-sm">Data</label>
+              <div className="scrollbar-thin scrollbar-track-dark scrollbar-thumb-darker mt-3 h-full h-full overflow-y-scroll rounded-md lg:max-h-64">
+                <CopyBlock
+                  text={JSON.stringify(auditLogs, null, 2)}
+                  language="json"
+                  theme={dracula}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </SlideOver>
+  );
+}
