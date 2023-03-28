4f063f9450ad54f646dec08496a0e59afda04d05
chetannn <chetankharel7@gmail.com>
2023-03-28 19:26:18 +0545

    feat: slide over and syntax highlighting for audit log detail

--- apps/platform/pages/projects/[slug]/audits.tsx
+++ apps/platform/pages/projects/[slug]/audits.tsx
@@ -1,11 +1,12 @@
 import { GetServerSidePropsContext } from "next";
 import Link from "next/link";
+import { useState } from "react";
 import ProjectLayout from "@/layouts/Project";
 import { withAccessControl } from "@/utils/withAccessControl";
 import { Project, UserRole } from "@prisma/client";
-import { Columns, Filter, Search } from "lucide-react";
+import { Columns, Download, Filter, Search } from "lucide-react";
+import { CopyBlock, dracula } from "react-code-blocks";
 import { BaseInput, Button, SlideOver } from "@/components/theme";
-import { useState } from "react";
 
 /**
  * A functional component that represents a project.
@@ -28,7 +29,6 @@ export const AuditLogsPage = ({
   currentRole,
   auditLogs,
 }: Props) => {
-
   const [open, setOpen] = useState(false);
 
   return (
@@ -38,23 +38,63 @@ export const AuditLogsPage = ({
       currentRole={currentRole}
       currentProject={currentProject}
     >
-
-      <SlideOver title="Audit log title" size="xl" description="description of the title" open={open} setOpen={setOpen} onClose={() => setOpen(false)}>
-        <div className="flex flex-col h-full">
+      <SlideOver
+        title="Audit Log Detail"
+        size="xl"
+        description="Chetan Kharel performed access.created on 29 July 2022, 21:32"
+        open={open}
+        setOpen={setOpen}
+        submitButtonIcon={
+          <Download className="ml-2 -mr-1 h-5 w-5" aria-hidden="true" />
+        }
+        submitButtonText="Download CSV"
+        onClose={() => setOpen(false)}
+      >
+        <div className="flex h-full flex-col">
           <div className="flex-1 overflow-y-auto">
-            <div className="py-6 px-4 sm:px-6">
-              <h2 className="text-2xl font-bold text-light">Audit log title</h2></div>
+            <div className="divide divide-dark divide-y-2 py-2 px-4 sm:px-6">
+              <div className="my-4 grid grid-cols-2 py-4 text-sm">
+                <label>Created By</label>
+                <div>Chetan Kharel</div>
               </div>
+
+              <div className="my-4 grid grid-cols-2 py-4 text-sm">
+                <label>Action Performed</label>
+                <div>
+                  <span className="border-dark rounded-xl border px-2 py-0.5 font-normal tracking-tight text-emerald-200">
+                    access.created
+                  </span>
+                </div>
               </div>
+
+              <div className="my-4 grid grid-cols-2 py-4 text-sm">
+                <label>Project</label>
+                <div>Untitled X</div>
+              </div>
+
+              <div className="my-4 grid grid-cols-2 py-4 text-sm">
+                <label>Created At</label>
+                <div>29 July 2022, 9:32 PM</div>
+              </div>
+
+              <div className="scrollbar-thin scrollbar-track-dark  scrollbar-thumb-darker h-full overflow-y-scroll rounded-md lg:max-h-64">
+                <CopyBlock
+                  text={JSON.stringify(auditLogs, null, 2)}
+                  language="json"
+                  theme={dracula}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </SlideOver>
 
       <div className="border-dark mt-12 w-full overflow-x-auto rounded-md border-2 shadow-sm">
         <div className="flex items-center justify-end py-2 px-2 font-medium">
           <div className="flex w-full items-center justify-end gap-2 md:max-w-md">
             <div className="flex flex-1 justify-end">
-              <div className="relative w-full group">
-
-                <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center justify-center pl-3 text-light group-focus-within:text-lighter">
+              <div className="group relative w-full">
+                <div className="text-light group-focus-within:text-lighter pointer-events-none absolute inset-y-0 left-0 flex items-center justify-center pl-3">
                   <Search className="h-4 w-4" />
                 </div>
 
@@ -83,23 +123,25 @@ export const AuditLogsPage = ({
           <thead className="border-dark border-b-2">
             <tr>
               <th className="py-3 px-6 text-sm font-medium">Created By</th>
-              <th className="py-3 px-6 text-sm font-normal">Action Performed</th>
+              <th className="py-3 px-6 text-sm font-normal">
+                Action Performed
+              </th>
               <th className="py-3 px-6 text-sm font-normal">Project</th>
               <th className="py-3 px-6 text-sm font-normal">Created At</th>
-              <th className="py-3 text-sm px-6"></th>
+              <th className="py-3 px-6 text-sm"></th>
             </tr>
           </thead>
-          <tbody className="divide-dark divide-y bg-darker">
+          <tbody className="divide-dark bg-darker divide-y">
             {auditLogs.map((item, idx) => (
               <tr key={idx}>
                 <td className="flex items-center gap-x-3 whitespace-nowrap py-3 px-6">
-                  <img src={item.avatar} className="w-10 h-10 rounded-full" />
+                  <img src={item.avatar} className="h-10 w-10 rounded-full" />
                   <div>
                     <span className="block text-xs">{item.createdBy}</span>
                   </div>
                 </td>
-                <td className="whitespace-nowrap rounded-md px-6 py-4 ">
-                  <span className="border-dark rounded-xl border px-2 py-0.5 tracking-tight uppercase text-emerald-200 font-normal">
+                <td className="whitespace-nowrap rounded-md px-6 py-4">
+                  <span className="border-dark rounded-xl border px-2 py-0.5 font-normal uppercase tracking-tight text-emerald-200">
                     {item.action}
                   </span>
                 </td>
@@ -161,7 +203,7 @@ const _getServerSideProps = async (context: GetServerSidePropsContext) => {
           createdBy: "Chetan Kharel",
           action: "access.created",
           project: "Untitled X",
-          avatar: "https://i.pravatar.cc/150?img=3"
+          avatar: "https://i.pravatar.cc/150?img=3",
         },
         {
           id: 1,
@@ -170,7 +212,7 @@ const _getServerSideProps = async (context: GetServerSidePropsContext) => {
           createdBy: "Chetan Kharel",
           action: "access.created",
           project: "Untitled X",
-          avatar: "https://i.pravatar.cc/150?img=3"
+          avatar: "https://i.pravatar.cc/150?img=3",
         },
         {
           id: 1,
@@ -179,8 +221,7 @@ const _getServerSideProps = async (context: GetServerSidePropsContext) => {
           createdBy: "Chetan Kharel",
           action: "access.created",
           project: "Untitled X",
-          avatar: "https://i.pravatar.cc/150?img=3"
-
+          avatar: "https://i.pravatar.cc/150?img=3",
         },
         {
           id: 1,
@@ -190,8 +231,7 @@ const _getServerSideProps = async (context: GetServerSidePropsContext) => {
           action: "access.created",
           project: "Untitled X",
 
-          avatar: "https://i.pravatar.cc/150?img=3"
-
+          avatar: "https://i.pravatar.cc/150?img=3",
         },
         {
           id: 1,
@@ -201,7 +241,7 @@ const _getServerSideProps = async (context: GetServerSidePropsContext) => {
           action: "access.created",
           project: "Untitled X",
 
-          avatar: "https://i.pravatar.cc/150?img=3"
+          avatar: "https://i.pravatar.cc/150?img=3",
         },
         {
           id: 1,
@@ -211,7 +251,7 @@ const _getServerSideProps = async (context: GetServerSidePropsContext) => {
           action: "access.created",
           project: "Untitled X",
 
-          avatar: "https://i.pravatar.cc/150?img=3"
+          avatar: "https://i.pravatar.cc/150?img=3",
         },
         {
           id: 1,
@@ -221,7 +261,7 @@ const _getServerSideProps = async (context: GetServerSidePropsContext) => {
           action: "access.created",
           project: "Untitled X",
 
-          avatar: "https://i.pravatar.cc/150?img=3"
+          avatar: "https://i.pravatar.cc/150?img=3",
         },
         {
           id: 1,
@@ -231,7 +271,7 @@ const _getServerSideProps = async (context: GetServerSidePropsContext) => {
           action: "access.created",
           project: "Untitled X",
 
-          avatar: "https://i.pravatar.cc/150?img=3"
+          avatar: "https://i.pravatar.cc/150?img=3",
         },
         {
           id: 1,
@@ -241,7 +281,7 @@ const _getServerSideProps = async (context: GetServerSidePropsContext) => {
           action: "access.created",
           project: "Untitled X",
 
-          avatar: "https://i.pravatar.cc/150?img=3"
+          avatar: "https://i.pravatar.cc/150?img=3",
         },
         {
           id: 1,
@@ -251,7 +291,7 @@ const _getServerSideProps = async (context: GetServerSidePropsContext) => {
           action: "access.created",
           project: "Untitled X",
 
-          avatar: "https://i.pravatar.cc/150?img=3"
+          avatar: "https://i.pravatar.cc/150?img=3",
         },
         {
           id: 1,
@@ -261,7 +301,7 @@ const _getServerSideProps = async (context: GetServerSidePropsContext) => {
           action: "access.created",
           project: "Untitled X",
 
-          avatar: "https://i.pravatar.cc/150?img=3"
+          avatar: "https://i.pravatar.cc/150?img=3",
         },
         {
           id: 1,
@@ -271,7 +311,7 @@ const _getServerSideProps = async (context: GetServerSidePropsContext) => {
           action: "access.created",
           project: "Untitled X",
 
-          avatar: "https://i.pravatar.cc/150?img=3"
+          avatar: "https://i.pravatar.cc/150?img=3",
         },
       ],
     },
