d044f8aaee8b0ec25d912adc9a47e6abff96de0b
chetannn <chetankharel7@gmail.com>
2023-03-28 20:34:00 +0545

    feat: loaded audit logs data and quick refactoring

--- apps/platform/pages/projects/[slug]/audits.tsx
+++ apps/platform/pages/projects/[slug]/audits.tsx
@@ -1,12 +1,10 @@
 import { GetServerSidePropsContext } from "next";
-import Link from "next/link";
 import { useState } from "react";
 import ProjectLayout from "@/layouts/Project";
 import { withAccessControl } from "@/utils/withAccessControl";
 import { Project, UserRole } from "@prisma/client";
-import { Columns, Download, Filter, Search } from "lucide-react";
-import { CopyBlock, dracula } from "react-code-blocks";
-import { BaseInput, Button, SlideOver } from "@/components/theme";
+import AuditLogSideOver from "@/components/projects/auditLogs/AuditLogSlideOver";
+import AuditLogTable from "@/components/projects/auditLogs/AuditLogTable";
 
 /**
  * A functional component that represents a project.
@@ -38,282 +36,35 @@ export const AuditLogsPage = ({
       currentRole={currentRole}
       currentProject={currentProject}
     >
-      <SlideOver
-        title="Audit Log Detail"
-        size="xl"
-        description="Chetan Kharel performed access.created on 29 July 2022, 21:32"
-        open={open}
-        setOpen={setOpen}
-        submitButtonIcon={
-          <Download className="ml-2 -mr-1 h-5 w-5" aria-hidden="true" />
-        }
-        submitButtonText="Download CSV"
-        onClose={() => setOpen(false)}
-      >
-        <div className="flex h-full flex-col">
-          <div className="flex-1 overflow-y-auto">
-            <div className="divide divide-dark divide-y-2 py-2 px-4 sm:px-6">
-              <div className="my-4 grid grid-cols-2 py-4 text-sm">
-                <label>Created By</label>
-                <div>Chetan Kharel</div>
-              </div>
-
-              <div className="my-4 grid grid-cols-2 py-4 text-sm">
-                <label>Action Performed</label>
-                <div>
-                  <span className="border-dark rounded-xl border px-2 py-0.5 font-normal tracking-tight text-emerald-200">
-                    access.created
-                  </span>
-                </div>
-              </div>
-
-              <div className="my-4 grid grid-cols-2 py-4 text-sm">
-                <label>Project</label>
-                <div>Untitled X</div>
-              </div>
-
-              <div className="my-4 grid grid-cols-2 py-4 text-sm">
-                <label>Created At</label>
-                <div>29 July 2022, 9:32 PM</div>
-              </div>
-
-              <div className="scrollbar-thin scrollbar-track-dark  scrollbar-thumb-darker h-full overflow-y-scroll rounded-md lg:max-h-64">
-                <CopyBlock
-                  text={JSON.stringify(auditLogs, null, 2)}
-                  language="json"
-                  theme={dracula}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </SlideOver>
-
-      <div className="border-dark mt-12 w-full overflow-x-auto rounded-md border-2 shadow-sm">
-        <div className="flex items-center justify-end py-2 px-2 font-medium">
-          <div className="flex w-full items-center justify-end gap-2 md:max-w-md">
-            <div className="flex flex-1 justify-end">
-              <div className="group relative w-full">
-                <div className="text-light group-focus-within:text-lighter pointer-events-none absolute inset-y-0 left-0 flex items-center justify-center pl-3">
-                  <Search className="h-4 w-4" />
-                </div>
-
-                <BaseInput
-                  placeholder="Search"
-                  className="w-full max-w-xs py-1.5 !pl-9"
-                  full={false}
-                />
-              </div>
-            </div>
-
-            <div className="flex shrink-0 items-center">
-              <button className="flex h-10 w-10 items-center justify-center rounded-full hover:bg-white/25">
-                <Filter className="h-5 w-5" />
-              </button>
-            </div>
-
-            <div className="flex shrink-0 items-center">
-              <button className="flex h-10 w-10 items-center justify-center rounded-full hover:bg-white/25">
-                <Columns className="h-5 w-5" />
-              </button>
-            </div>
-          </div>
-        </div>
-        <table className="w-full table-auto border-b-2 text-left text-sm">
-          <thead className="border-dark border-b-2">
-            <tr>
-              <th className="py-3 px-6 text-sm font-medium">Created By</th>
-              <th className="py-3 px-6 text-sm font-normal">
-                Action Performed
-              </th>
-              <th className="py-3 px-6 text-sm font-normal">Project</th>
-              <th className="py-3 px-6 text-sm font-normal">Created At</th>
-              <th className="py-3 px-6 text-sm"></th>
-            </tr>
-          </thead>
-          <tbody className="divide-dark bg-darker divide-y">
-            {auditLogs.map((item, idx) => (
-              <tr key={idx}>
-                <td className="flex items-center gap-x-3 whitespace-nowrap py-3 px-6">
-                  <img src={item.avatar} className="h-10 w-10 rounded-full" />
-                  <div>
-                    <span className="block text-xs">{item.createdBy}</span>
-                  </div>
-                </td>
-                <td className="whitespace-nowrap rounded-md px-6 py-4">
-                  <span className="border-dark rounded-xl border px-2 py-0.5 font-normal uppercase tracking-tight text-emerald-200">
-                    {item.action}
-                  </span>
-                </td>
-                <td className="whitespace-nowrap px-6 py-4">{item.project}</td>
-                <td className="whitespace-nowrap px-6 py-4">
-                  {item.createdAt}
-                </td>
-                <td className="whitespace-nowrap px-6 text-right">
-                  <button
-                    onClick={() => setOpen(true)}
-                    className="py-2 px-3 font-medium text-teal-400 duration-150 hover:text-teal-300 hover:underline"
-                  >
-                    Detail
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-
-        <div className="flex items-center justify-between py-2 px-2 font-medium">
-          <p className="text-xs">100 items</p>
-          <div className="flex items-center gap-4">
-            <Link className="text-xs text-teal-400" href="/">
-              Prev
-            </Link>
-            <div>
-              <span className="block text-xs">Page 1 of 3</span>
-            </div>
-            <Link className="text-xs text-teal-400" href="/">
-              Next
-            </Link>
-          </div>
-
-          <div className="flex items-center gap-3 text-xs">
-            <p>Per Page</p>
-            <select className="input-primary text-xs">
-              <option>10</option>
-              <option>20</option>
-              <option>30</option>
-              <option>40</option>
-              <option>50</option>
-            </select>
-          </div>
-        </div>
-      </div>
+      <AuditLogSideOver open={open} setOpen={setOpen} auditLogs={auditLogs} />
+      <AuditLogTable auditLogs={auditLogs} setSlideOverOpen={setOpen} />
     </ProjectLayout>
   );
 };
 
 const _getServerSideProps = async (context: GetServerSidePropsContext) => {
+  const auditLogs = await prisma?.audit.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      project: {
+        select: {
+          name: true,
+        },
+      },
+      createdBy: {
+        select: {
+          name: true,
+        },
+      },
+    },
+    take: 10,
+  });
+
   return {
     props: {
-      auditLogs: [
-        {
-          id: 1,
-          name: "audit log 1",
-          createdAt: "Mar 27, 2023, 6:24 AM",
-          createdBy: "Chetan Kharel",
-          action: "access.created",
-          project: "Untitled X",
-          avatar: "https://i.pravatar.cc/150?img=3",
-        },
-        {
-          id: 1,
-          name: "audit log 1",
-          createdAt: "Mar 27, 2023 6:24 AM",
-          createdBy: "Chetan Kharel",
-          action: "access.created",
-          project: "Untitled X",
-          avatar: "https://i.pravatar.cc/150?img=3",
-        },
-        {
-          id: 1,
-          name: "audit log 1",
-          createdAt: "Mar 27, 2023, 6:24 AM",
-          createdBy: "Chetan Kharel",
-          action: "access.created",
-          project: "Untitled X",
-          avatar: "https://i.pravatar.cc/150?img=3",
-        },
-        {
-          id: 1,
-          name: "audit log 1",
-          createdAt: "Mar 27, 2023, 6:24 AM",
-          createdBy: "Chetan Kharel",
-          action: "access.created",
-          project: "Untitled X",
-
-          avatar: "https://i.pravatar.cc/150?img=3",
-        },
-        {
-          id: 1,
-          name: "audit log 1",
-          createdAt: "2021-01-01",
-          createdBy: "Chetan Kharel",
-          action: "access.created",
-          project: "Untitled X",
-
-          avatar: "https://i.pravatar.cc/150?img=3",
-        },
-        {
-          id: 1,
-          name: "audit log 1",
-          createdAt: "2021-01-01",
-          createdBy: "Chetan Kharel",
-          action: "access.created",
-          project: "Untitled X",
-
-          avatar: "https://i.pravatar.cc/150?img=3",
-        },
-        {
-          id: 1,
-          name: "audit log 1",
-          createdAt: "2021-01-01",
-          createdBy: "Chetan Kharel",
-          action: "access.created",
-          project: "Untitled X",
-
-          avatar: "https://i.pravatar.cc/150?img=3",
-        },
-        {
-          id: 1,
-          name: "audit log 1",
-          createdAt: "2021-01-01",
-          createdBy: "Chetan Kharel",
-          action: "access.created",
-          project: "Untitled X",
-
-          avatar: "https://i.pravatar.cc/150?img=3",
-        },
-        {
-          id: 1,
-          name: "audit log 1",
-          createdAt: "2021-01-01",
-          createdBy: "Chetan Kharel",
-          action: "access.created",
-          project: "Untitled X",
-
-          avatar: "https://i.pravatar.cc/150?img=3",
-        },
-        {
-          id: 1,
-          name: "audit log 1",
-          createdAt: "2021-01-01",
-          createdBy: "Chetan Kharel",
-          action: "access.created",
-          project: "Untitled X",
-
-          avatar: "https://i.pravatar.cc/150?img=3",
-        },
-        {
-          id: 1,
-          name: "audit log 1",
-          createdAt: "2021-01-01",
-          createdBy: "Chetan Kharel",
-          action: "access.created",
-          project: "Untitled X",
-
-          avatar: "https://i.pravatar.cc/150?img=3",
-        },
-        {
-          id: 1,
-          name: "audit log 1",
-          createdAt: "2021-01-01",
-          createdBy: "Chetan Kharel",
-          action: "access.created",
-          project: "Untitled X",
-
-          avatar: "https://i.pravatar.cc/150?img=3",
-        },
-      ],
+      auditLogs: JSON.parse(JSON.stringify(auditLogs)),
     },
   };
 };
