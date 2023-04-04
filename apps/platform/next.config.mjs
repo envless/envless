3bf749cd90e64fa9a470a54d1c195b4901d2e882
aju100 <ajutamang10@outlook.com>
2023-04-04 17:50:06 +0545

    fix: set SKIP_ENV_VALIDATION false

--- apps/platform/next.config.mjs
+++ apps/platform/next.config.mjs
@@ -2,7 +2,7 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
-!process.env.SKIP_ENV_VALIDATION && (await import("./env/index.mjs"));
+//!process.env.SKIP_ENV_VALIDATION && (await import("./env/index.mjs"));
 
 const nextConfig = {
   reactStrictMode: true,
