68eead30afe38333d2c0588a5842fb6e64b71c01
swapnil-talpade <swapniltalpade41@gmail.com>
2023-04-09 20:39:27 +0530

    chore: returns image if member has an image

--- apps/platform/utils/avatar-generator.ts
+++ apps/platform/utils/avatar-generator.ts
@@ -3,6 +3,8 @@ import md5 from "md5";
 import { getInitials } from "./helpers";
 
 export const getAvatar = (member: UserType) => {
+  if (member.image) return member.image;
+
   const hash = md5(member.email);
 
   const defaultAvatar = `https://avatar.vercel.sh/${getInitials(
