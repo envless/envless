bb6a62165d6a630467d05aa783a611922376da51
Chinedu Ezeh <chineduezeh19@gmail.com>
2023-04-12 12:10:01 +0100

    chore: made slug field optional & auto-generated

--- apps/cli/src/commands/project/create.ts
+++ apps/cli/src/commands/project/create.ts
@@ -1,6 +1,6 @@
 import { intro, isCancel, outro, select, spinner, text } from "@clack/prompts";
 import { Args, Command, Flags } from "@oclif/core";
-import axios, { AxiosError, AxiosResponse } from "axios";
+import axios, { Axios, AxiosError, AxiosResponse } from "axios";
 import { blue, bold, cyan, grey, red, underline } from "kleur/colors";
 import { triggerCancel } from "../../lib/helpers";
 import { getCliConfigFromKeyStore } from "../../lib/keyStore";
@@ -11,24 +11,28 @@ type NewProjectResponse = {
   createdAt: Date;
 };
 
+type ErrorMessage = {
+  message: string;
+};
+
 const loader = spinner();
 const API_BASE = process.env.API_BASE || `http://localhost:3000`;
 const LINKS = {
   projectsPage: `${API_BASE}/projects`,
   documentation: `${API_BASE}/docs/cli/projects`,
-  auth: `${API_BASE}/cli/auth`,
+  projects: `${API_BASE}/cli/projects`,
 };
 
 export default class ProjectCreate extends Command {
   static description = `Create a new Envless project.\n${grey(
-    `⚡ Please make sure you are running this command from your development project's root directory and make sure you're authenticated. If not, please do so at \n${cyan(
-      LINKS.auth,
+    `⚡ Please make sure you are running this command from your project's root directory and make sure you have completed your CLI setup. If not, please do so at \n${cyan(
+      LINKS.projects,
     )}`,
   )}`;
   static examples = [
     `
       $ envless project create
-      $ envless project create {project name} {project slug}
+      $ envless project create {project name}
       `,
   ];
 
@@ -36,16 +40,10 @@ export default class ProjectCreate extends Command {
     name: Flags.string({
       description: `This is the project's name`,
       char: "n",
-      dependsOn: ["slug"],
-    }),
-    slug: Flags.string({
-      description: `This will be used as the slug for the project`,
-      char: "s",
-      dependsOn: ["name"],
     }),
   };
 
-  async createProject(projectName: string, projectSlug: string) {
+  private async createProject(projectName: string) {
     await loader.start(
       `Creating your Envless project: ${bold(cyan(projectName))}...`,
     );
@@ -67,7 +65,6 @@ export default class ProjectCreate extends Command {
       const response = await axios.post(
         `${API_BASE}/api/cli/v0/projects/create`,
         {
-          slug: projectSlug,
           name: projectName,
         },
         {
@@ -87,7 +84,16 @@ export default class ProjectCreate extends Command {
           blue(`${LINKS.projectsPage}/${data.slug}`),
         )}`,
       );
-    } catch (error) {
+    } catch (err: any) {
+      const error = err as AxiosError;
+      if (error.response?.status === 409) {
+        loader.stop(
+          red(`Project with name: ${bold(projectName)} already exists`),
+        );
+        triggerCancel();
+        return;
+      }
+      console.log(err);
       loader.stop(`Unauthorized error while creating the project`);
       triggerCancel();
     }
@@ -98,7 +104,7 @@ export default class ProjectCreate extends Command {
     const { flags } = await this.parse(ProjectCreate);
     this.log(`Envless CLI ${grey(`${version}`)}`);
 
-    if (!flags.name || !flags.slug) {
+    if (!flags.name) {
       const projectName: any = await text({
         message: `Enter project name:`,
         validate: (input: string) => {
@@ -107,21 +113,14 @@ export default class ProjectCreate extends Command {
           }
         },
       });
-      const projectSlug: any = await text({
-        message: `Enter project slug:`,
-        validate: (input: string) => {
-          if (!input || input.trim() === "" || input.trim().length < 3) {
-            return `Please enter a valid Project name`;
-          }
-        },
-      });
-      if (projectName && projectSlug) {
-        this.createProject(projectName, projectSlug);
+
+      if (projectName) {
+        this.createProject(projectName);
         return;
       }
     } else {
-      if (flags.name && flags.slug) {
-        this.createProject(flags.name, flags.slug);
+      if (flags.name) {
+        this.createProject(flags.name);
         return;
       }
     }
@@ -129,9 +128,7 @@ export default class ProjectCreate extends Command {
       `Use the command ${cyan(
         "envless project create",
       )} to get prompt or ${cyan(
-        `envless project create ${bold(
-          '--name "[PROJECT_NAME]" --slug "[PROJECT_SLUG]"',
-        )}`,
+        `envless project create ${bold('--name "[PROJECT_NAME]"')}`,
       )} to create a project`,
     );
     triggerCancel();
--- apps/platform/pages/api/cli/v0/projects/create.ts
+++ apps/platform/pages/api/cli/v0/projects/create.ts
@@ -20,82 +20,115 @@ type NewProjectResponse = {
   createdAt: Date;
 };
 
+const doesProjectConflict = async (name: string, slug: string) => {
+  const project = await prisma.project.findFirst({
+    where: {
+      OR: [
+        {
+          name,
+        },
+        { slug },
+      ],
+    },
+  });
+  if (project) {
+    const conflictField = project.slug === slug ? "slug" : "name";
+    if (conflictField == "slug") {
+      const newSlug = `slug-${Date.now()}`;
+      doesProjectConflict(name, newSlug);
+    }
+    return { conflictField, project: { name, slug } };
+  } else if (project === null) {
+    return { conflictField: null, project: { name, slug } };
+  }
+};
+
 const createEnvlessProject = async (
   project: { name: string; slug: string },
   { user }: NextCliApiRequest,
+  res: NextApiResponse,
 ) => {
-  const newProject = await prisma.project.create({
-    data: {
-      name: project.name,
-      slug: kebabCase(project.slug),
-      access: {
-        create: {
-          userId: user.id,
-          role: "owner",
-        },
-      },
-      branches: {
-        create: {
-          name: "main",
-          protected: true,
-          description: "Main branch is protected by default",
-          protectedAt: new Date(),
-          createdById: user.id,
-        },
-      },
-    },
-
-    include: {
-      access: true,
-      branches: true,
-    },
-  });
-
-  if (newProject.id) {
-    await Audit.create({
-      createdById: user.id,
-      projectId: newProject.id,
-      action: PROJECT_CREATED,
-      data: {
-        project: {
-          id: newProject.id,
-          name: newProject.name,
-        },
-      },
+  const checkConflict = await doesProjectConflict(project.name, project.slug);
+  if (checkConflict?.conflictField) {
+    res.status(409).json({
+      message: `Project with name ${checkConflict.project.name} already exists`,
     });
-
-    // @ts-ignore
-    const access = newProject.access[0];
-    // @ts-ignore
-    const branch = newProject.branches[0];
-
-    await Audit.create({
-      createdById: user.id,
-      createdForId: user.id,
-      projectId: newProject.id,
-      action: ACCESS_CREATED,
-      data: {
-        access: {
-          id: access.id,
-          role: access.role,
-        },
-      },
-    });
-
-    await Audit.create({
-      createdById: user.id,
-      projectId: newProject.id,
-      action: BRANCH_CREATED,
-      data: {
-        branch: {
-          id: branch.id,
-          name: branch.name,
-        },
-      },
-    });
+    return false;
   }
+  if (!checkConflict?.conflictField && checkConflict?.project) {
+    const newProject = await prisma.project.create({
+      data: {
+        name: checkConflict.project.name,
+        slug: checkConflict.project.slug,
+        access: {
+          create: {
+            userId: user.id,
+            role: "owner",
+          },
+        },
+        branches: {
+          create: {
+            name: "main",
+            protected: true,
+            description: "Main branch is protected by default",
+            protectedAt: new Date(),
+            createdById: user.id,
+          },
+        },
+      },
 
-  return newProject;
+      include: {
+        access: true,
+        branches: true,
+      },
+    });
+
+    if (newProject.id) {
+      await Audit.create({
+        createdById: user.id,
+        projectId: newProject.id,
+        action: PROJECT_CREATED,
+        data: {
+          project: {
+            id: newProject.id,
+            name: newProject.name,
+          },
+        },
+      });
+
+      // @ts-ignore
+      const access = newProject.access[0];
+      // @ts-ignore
+      const branch = newProject.branches[0];
+
+      await Audit.create({
+        createdById: user.id,
+        createdForId: user.id,
+        projectId: newProject.id,
+        action: ACCESS_CREATED,
+        data: {
+          access: {
+            id: access.id,
+            role: access.role,
+          },
+        },
+      });
+
+      await Audit.create({
+        createdById: user.id,
+        projectId: newProject.id,
+        action: BRANCH_CREATED,
+        data: {
+          branch: {
+            id: branch.id,
+            name: branch.name,
+          },
+        },
+      });
+    }
+
+    return newProject;
+  }
 };
 
 const create = async (
@@ -108,19 +141,21 @@ const create = async (
 
   //   Project Info
   const projectName = req.body.name as string;
-  const projectSlug = req.body.slug as string;
 
-  if (!projectName || !projectSlug) {
+  if (!projectName) {
     res.status(400).json({
       message: "Params to create project is not complete",
     } as ErrorMessage);
     return;
   }
 
+  const projectSlug = kebabCase(projectName);
+
   try {
     const newProject = await createEnvlessProject(
       { name: projectName, slug: projectSlug },
       req,
+      res,
     );
     if (newProject) {
       res.status(200).json({
