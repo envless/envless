c2bbbff94681074a8587bd5d08897c53d3312a8f
Puru D <puru@dahal.me>
2023-04-13 00:57:34 -0500

    feat: implement swagger for CLI api

--- apps/platform/pages/api/cli/v1/projects/index.ts
+++ apps/platform/pages/api/cli/v1/projects/index.ts
@@ -2,6 +2,30 @@ import type { NextApiResponse } from "next";
 import { createProject, getProjects } from "@/utils/api/projects.controller";
 import withCliAuth, { type NextCliApiRequest } from "@/utils/withCliAuth";
 
+/**
+ * @swagger
+ * /api/cli/v1/projects:
+ *  get:
+ *    description: Get all projects
+ *    responses:
+ *      200:
+ *        description: Success
+ *    content:
+ *        application/json:
+ *          schema:
+ *            type: array
+ *            projects:
+ *              type: object
+ *              properties:
+ *                id: string
+ *                name: string
+ *    post:
+ *      description: Create a new project
+ *      responses:
+ *        200:
+ *          description: Success
+ */
+
 const projects = async (req: NextCliApiRequest, res: NextApiResponse) => {
   if (req.method === "GET") {
     getProjects(req, res);
