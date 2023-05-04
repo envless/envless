bafc634c048d4abaf2f50ebdb783b96494c6c1b7
chetannn <chetankharel7@gmail.com>
2023-05-04 17:31:38 +0545

    feat: clone all the base branch secrets when new branch created

--- apps/platform/components/branches/CreateBranchModal.tsx
+++ apps/platform/components/branches/CreateBranchModal.tsx
@@ -1,12 +1,12 @@
 import { useRouter } from "next/router";
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useEffect } from "react";
 import { useZodForm } from "@/hooks/useZodForm";
 import { useBranchesStore } from "@/store/Branches";
 import { trpc } from "@/utils/trpc";
 import { Branch, Project } from "@prisma/client";
 import { AlertCircle } from "lucide-react";
 import { SubmitHandler } from "react-hook-form";
-import * as z from "zod";
+import { z } from "zod";
 import BranchComboBox from "@/components/branches/BranchComboBox";
 import {
   BaseInput,
@@ -45,16 +45,26 @@ const CreateBranchModal = ({
         /^[a-z0-9][a-z0-9-]{0,}[a-z0-9]$/,
         "Name can only contain lowercase alphanumeric characters and dashes, cannot start or end with a dash, and must be at least two characters.",
       ),
+    baseBranchId: z.string().min(1, { message: "Base Branch is required" }),
   });
+
+  type BranchFormSchema = z.infer<typeof schema>;
 
   const {
     reset,
     handleSubmit,
     register,
+    setValue,
     formState: { errors },
   } = useZodForm({
     schema,
   });
+
+  useEffect(() => {
+    if (baseBranch) {
+      setValue("baseBranchId", baseBranch.id);
+    }
+  }, [setValue, baseBranch]);
 
   const branchMutation = trpc.branches.create.useMutation({
     onSuccess: (data: Branch) => {
@@ -78,7 +88,7 @@ const CreateBranchModal = ({
     },
   });
 
-  const createNewBranch: SubmitHandler<Project> = async (data) => {
+  const createNewBranch: SubmitHandler<BranchFormSchema> = async (data) => {
     const { name } = data;
 
     if (!name) {
@@ -87,7 +97,9 @@ const CreateBranchModal = ({
 
     const projectSlug = router.query.slug as string;
 
-    branchMutation.mutate({ branch: { name: name, projectSlug } });
+    branchMutation.mutate({
+      branch: { name: name, projectSlug, baseBranchId: baseBranch.id },
+    });
   };
 
   return (
