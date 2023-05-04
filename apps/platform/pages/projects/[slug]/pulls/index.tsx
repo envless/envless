1d19e33de3641adcc8dbb73d3633d373cd4ae153
Puru D <puru@dahal.me>
2023-05-04 16:22:10 -0700

    chore: remove un-used code and some typesafety issue

--- apps/platform/pages/projects/[slug]/pulls/index.tsx
+++ apps/platform/pages/projects/[slug]/pulls/index.tsx
@@ -59,7 +59,11 @@ export const PullRequestPage = ({
   );
 
   const pullRequestColumns: ColumnDef<
-    PullRequest & { createdBy: User; baseBranch: Branch; currentBranch: Branch }
+    PullRequest & {
+      createdBy: User;
+      baseBranch: { id: string; name: string };
+      currentBranch: { id: string; name: string };
+    }
   >[] = [
     {
       id: "title",
@@ -235,55 +239,3 @@ export const getServerSideProps = withAccessControl({
 });
 
 export default PullRequestPage;
-
-interface PullRequestHoverCardProps {
-  triggerComponent: ReactNode;
-}
-
-function PullRequestHoverCard({ triggerComponent }: PullRequestHoverCardProps) {
-  return (
-    <HoverCard.Root>
-      <HoverCard.Trigger asChild>{triggerComponent}</HoverCard.Trigger>
-
-      <HoverCard.Portal>
-        <HoverCard.Content
-          className="bg-darker w-[350px] rounded text-xs"
-          sideOffset={5}
-        >
-          <div className="flex w-full flex-col gap-[10px] px-3 py-4">
-            <div className="text-light">
-              <Link href={"#"} className="underline">
-                envless/envless
-              </Link>{" "}
-              on Feb 22
-            </div>
-
-            <div className="flex items-start gap-[10px]">
-              <div className="shrink-0">
-                <GitPullRequest className="h-4 w-4 text-emerald-200" />
-              </div>
-
-              <div className="flex flex-col">
-                <p className="text-md font-bold">
-                  feat: additional security - ask users to provide OTP for one
-                  last time before they disable two factor auth{" "}
-                </p>
-                <div className="mt-2 inline-flex items-center gap-2">
-                  <span className="bg-dark text-light rounded px-1 py-0.5">
-                    envless:main
-                  </span>
-                  <ArrowLeft className="text-lighter h-4 w-4 shrink-0" />
-                  <span className="bg-dark text-light rounded px-1 py-0.5">
-                    samyogdhital:proj...
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <HoverCard.Arrow className="text-dark" />
-        </HoverCard.Content>
-      </HoverCard.Portal>
-    </HoverCard.Root>
-  );
-}
