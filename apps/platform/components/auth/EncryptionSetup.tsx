e9029b82c9e8943884d5523406fa535de6b6fb9d
Puru D <puru@dahal.me>
2023-06-02 03:07:08 -0500

    chore: template for encryption setup

--- apps/platform/components/auth/EncryptionSetup.tsx
+++ apps/platform/components/auth/EncryptionSetup.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import { KeychainType, UserType } from "@/types/resources";
@@ -217,18 +218,44 @@ const EncryptionSetup = ({
   return (
     <>
       <div className="flex flex-col px-5 py-32">
-        <div className="bg-darker rounded-md px-5 py-12 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="bg-darker rounded-md px-10 py-12 sm:mx-auto sm:w-full sm:max-w-md">
           <div className="">
             <EncryptionIcon className="mx-auto mb-3 h-16 w-16 text-teal-400" />
             <h2 className="mt-6 text-center text-2xl">
-              {page == "setupKeychain" ? "Enter" : "Setup"} master password
+              Setup end-to-end encryption
             </h2>
             <p className="text-light mt-2 text-center text-sm">
-              Master password is required for additional security.
+              Generate and download your encryption keys. Encryption keys are
+              generated on the client side and never saved on the database,
+              encrypted or otherwise. We recommend you further encrypt and store
+              this private key to your most trusted password manager (eg.
+              BitWarden) or on a safe place. Secrets cannot be decrypted without
+              this key and you will need this this key everytime you login.
             </p>
           </div>
 
-          <div className="">
+          <div className="mt-8">
+            <Button
+              sr={"Generate and download encryption keys"}
+              type="submit"
+              width="full"
+              disabled={loading}
+            >
+              Generate and download encryption keys
+            </Button>
+          </div>
+
+          <div className="mt-3 text-center">
+            <Link
+              href="https://envless.dev/docs/encryption"
+              target="_blank"
+              className="text-xs text-teal-400 hover:text-teal-600"
+            >
+              How does end-to-end encryption work?
+            </Link>
+          </div>
+
+          {/* <div className="">
             <div className="px-2 py-8 sm:px-8">
               <Hr />
               <form
@@ -314,7 +341,7 @@ const EncryptionSetup = ({
                 </Button>
               </form>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
       <Toaster position="top-right" />
