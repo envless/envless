c6197340a5d036d2a0fe304059a5e4a104ebf3dd
Puru D <puru@dahal.me>
2023-06-21 23:27:14 -0500

    feat: keeping pages lean, single responsibility pages that handles only what its supposed to.

---
+++ apps/platform/pages/auth/otp.tsx
@@ -0,0 +1,65 @@
+import { UserType } from "@/types/resources";
+import { getServerSideSession } from "@/utils/session";
+import type { Keychain, User } from "@prisma/client";
+import { getCsrfToken } from "next-auth/react";
+import OneTimePassword from "@/components/auth/OneTimePassword";
+import prisma from "@/lib/prisma";
+
+type PageProps = {
+  currentUser: User;
+  csrfToken: string;
+  keychain: Keychain;
+};
+
+const OtpPage = ({ currentUser, keychain, csrfToken }: PageProps) => {
+  return (
+    <OneTimePassword
+      keychain={keychain}
+      csrfToken={csrfToken}
+      currentUser={currentUser}
+    />
+  );
+};
+
+export async function getServerSideProps(context) {
+  const session = await getServerSideSession(context);
+  const user = session?.user as UserType;
+
+  if (!session || !session.user) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
+
+  const currentUser = await prisma.user.findUnique({
+    where: { id: user.id },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      hashedPassword: true,
+      keychain: {
+        select: {
+          publicKey: true,
+          verificationString: true,
+        },
+      },
+    },
+  });
+
+  const keychain = currentUser?.keychain || null;
+  const csrfToken = await getCsrfToken(context);
+
+  return {
+    props: {
+      keychain,
+      csrfToken,
+      currentUser,
+    },
+  };
+}
+
+export default OtpPage;
