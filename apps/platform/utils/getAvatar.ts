cf6e91f14e22a899e7ab77c3ffabc1b03c77d6eb
Qutluq <kootlook.applications@gmail.com>
2023-07-21 06:48:46 +0600

    fix: md5 function does not accept undefined value

--- apps/platform/utils/getAvatar.ts
+++ apps/platform/utils/getAvatar.ts
@@ -6,7 +6,7 @@ import { getInitials } from "./helpers";
 export const getAvatar = (member: MemberType) => {
   if (member.image) return member.image;
 
-  const hash = md5(member.email);
+  const hash = md5(member.email || "");
   const initials = getInitials(member.name || member.email);
   const defaultAvatar = `${env.NEXT_PUBLIC_BASE_URL}/api/avatar/${initials}.svg?text=${initials}`;
   const avatar = `https://www.gravatar.com/avatar/${hash}?d=${defaultAvatar}`;
